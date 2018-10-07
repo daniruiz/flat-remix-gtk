b8bd3ca540c72458cf4126c420d2c161a31d2f78
Daniel Ruiz de Alegría <daniruizdealegria@gmail.com>
2018-10-07 17:25:37 +0200

    Add color gradients; round borders

---
+++ sass/fast-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ASSETS_DIR=../assets-renderer/gtk3/assets
+
+sass gtk.scss > gtk.css
+sass gtk-dark.scss > gtk-dark.css
+sass gtk-darker.scss > gtk-darker.css
+
+
+cp -f gtk.css 		../Flat-Remix-GTK/gtk-3.0/gtk.css
+cp -f gtk-dark.css 	../Flat-Remix-GTK/gtk-3.0/gtk-dark.css
+
+cp -f gtk-dark.css 	../Flat-Remix-GTK-Dark/gtk-3.0/gtk.css
+
+cp -f gtk-darker.css 	../Flat-Remix-GTK-Darker/gtk-3.0/gtk.css
+cp -f gtk-dark.css 	../Flat-Remix-GTK-Darker/gtk-3.0/gtk-dark.css
+
+if [ "$(ls -A $ASSETS_DIR)" ]
+then
+	rm $ASSETS_DIR/*
+fi
+../assets-renderer/gtk3/render-assets.sh
+
+rm -rf ../Flat-Remix-GTK/gtk-3.0/assets
+cp -rf $ASSETS_DIR ../Flat-Remix-GTK/gtk-3.0/assets
+
+rm -rf ../Flat-Remix-GTK-Dark/gtk-3.0/assets
+cp -rf $ASSETS_DIR ../Flat-Remix-GTK-Dark/gtk-3.0/assets
+
+rm -rf ../Flat-Remix-GTK-Darker/gtk-3.0/assets
+cp -rf $ASSETS_DIR ../Flat-Remix-GTK-Darker/gtk-3.0/assets
+
+
+sudo cp -R ../Flat-Remix-GTK* /usr/share/themes
+sudo killall nautilus
+nautilus &
