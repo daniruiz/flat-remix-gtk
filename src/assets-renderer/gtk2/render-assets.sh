9f77af20dad7f700b2451633e136896729feeeb8
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-03-17 19:39:29 +0100

    Update gtk2 assets renderer

--- src/assets-renderer/gtk2/render-assets.sh
+++ src/assets-renderer/gtk2/render-assets.sh
@@ -1,22 +1,26 @@
-#! /bin/bash
+#!/bin/bash
 
-INKSCAPE="/usr/bin/inkscape"
-OPTIPNG="/usr/bin/optipng"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
+INKSCAPE=/usr/bin/inkscape
+OPTIPNG=/usr/bin/optipng
 
-DARK_SRC_FILE="assets-dark.svg"
-DARK_ASSETS_DIR="assets-dark"
+SRC_FILE="$DIR"/assets.svg
+ASSETS_DIR="$DIR"/assets
 
-DARKEST_SRC_FILE="assets-darkest.svg"
-DARKEST_ASSETS_DIR="assets-darkest"
+SRC_FILE_DARK="$DIR"/assets-dark.svg
+ASSETS_DIR_DARK="$DIR"/assets-dark
 
-INDEX="assets.txt"
+SRC_FILE_DARKEST="$DIR"/assets-darkest.svg
+ASSETS_DIR_DARKEST="$DIR"/assets-darkest
 
-mkdir assets assets-dark assets-darkest menubar-toolbar
+MENU_TOOLBAR_DIR="$DIR"/menubar-toolbar
 
-for i in `cat $INDEX`
+INDEX="$DIR"/"assets.txt"
+
+mkdir "$ASSETS_DIR" "$ASSETS_DIR_DARK" "$ASSETS_DIR_DARKEST" "$MENU_TOOLBAR_DIR"
+
+for i in $(cat $INDEX)
 do
 	if [ -f $ASSETS_DIR/$i.png ]; then
 		echo $ASSETS_DIR/$i.png exists.
@@ -25,58 +29,57 @@ do
 		echo Rendering $ASSETS_DIR/$i.png
 		$INKSCAPE --export-id=$i \
 		          --export-id-only \
-		          --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+		          --export-png=$ASSETS_DIR/$i.png $SRC_FILE &> /dev/null \
 		&& $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
 	fi
 
-	if [ -f $DARK_ASSETS_DIR/$i.png ]; then
-		echo $DARK_ASSETS_DIR/$i.png exists.
+	if [ -f $ASSETS_DIR_DARK/$i.png ]; then
+		echo $ASSETS_DIR_DARK/$i.png exists.
 	else
 		echo
-		echo Rendering $DARK_ASSETS_DIR/$i.png
+		echo Rendering $ASSETS_DIR_DARK/$i.png
 		$INKSCAPE --export-id=$i \
 		          --export-id-only \
-		          --export-png=$DARK_ASSETS_DIR/$i.png $DARK_SRC_FILE >/dev/null \
-		&& $OPTIPNG -o7 --quiet $DARK_ASSETS_DIR/$i.png 
+		          --export-png=$ASSETS_DIR_DARK/$i.png $SRC_FILE_DARK &> /dev/null \
+		&& $OPTIPNG -o7 --quiet $ASSETS_DIR_DARK/$i.png 
 	fi
 
-	if [ -f $DARKEST_ASSETS_DIR/$i.png ]; then
-		echo $DARKEST_ASSETS_DIR/$i.png exists.
+	if [ -f $ASSETS_DIR_DARKEST/$i.png ]; then
+		echo $ASSETS_DIR_DARKEST/$i.png exists.
 	else
 		echo
-		echo Rendering $DARKEST_ASSETS_DIR/$i.png
+		echo Rendering $ASSETS_DIR_DARKEST/$i.png
 		$INKSCAPE --export-id=$i \
 		          --export-id-only \
-		          --export-png=$DARKEST_ASSETS_DIR/$i.png $DARKEST_SRC_FILE >/dev/null \
-		&& $OPTIPNG -o7 --quiet $DARKEST_ASSETS_DIR/$i.png 
+		          --export-png=$ASSETS_DIR_DARKEST/$i.png $SRC_FILE_DARKEST &> /dev/null \
+		&& $OPTIPNG -o7 --quiet $ASSETS_DIR_DARKEST/$i.png 
 	fi
 done
 
-cp $ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar.png
-cp $ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar.png
-cp $ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar.png
+cp $ASSETS_DIR/entry-toolbar.png "$MENU_TOOLBAR_DIR"/entry-toolbar.png
+cp $ASSETS_DIR/entry-active-toolbar.png "$MENU_TOOLBAR_DIR"/entry-active-toolbar.png
+cp $ASSETS_DIR/entry-disabled-toolbar.png "$MENU_TOOLBAR_DIR"/entry-disabled-toolbar.png
 
-cp $ASSETS_DIR/menubar.png menubar-toolbar/menubar.png
-cp $ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button.png
+cp $ASSETS_DIR/menubar.png "$MENU_TOOLBAR_DIR"/menubar.png
+cp $ASSETS_DIR/menubar_button.png "$MENU_TOOLBAR_DIR"/menubar_button.png
 
 
-cp $DARK_ASSETS_DIR/button.png menubar-toolbar/button.png
-cp $DARK_ASSETS_DIR/button-hover.png menubar-toolbar/button-hover.png
-cp $DARK_ASSETS_DIR/button-active.png menubar-toolbar/button-active.png
-cp $DARK_ASSETS_DIR/button-insensitive.png menubar-toolbar/button-insensitive.png
+cp $ASSETS_DIR_DARK/button.png "$MENU_TOOLBAR_DIR"/button.png
+cp $ASSETS_DIR_DARK/button-hover.png "$MENU_TOOLBAR_DIR"/button-hover.png
+cp $ASSETS_DIR_DARK/button-active.png "$MENU_TOOLBAR_DIR"/button-active.png
+cp $ASSETS_DIR_DARK/button-insensitive.png "$MENU_TOOLBAR_DIR"/button-insensitive.png
 
-cp $DARK_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar-dark.png
-cp $DARK_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar-dark.png
-cp $DARK_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar-dark.png
+cp $ASSETS_DIR_DARK/entry-toolbar.png "$MENU_TOOLBAR_DIR"/entry-toolbar-dark.png
+cp $ASSETS_DIR_DARK/entry-active-toolbar.png "$MENU_TOOLBAR_DIR"/entry-active-toolbar-dark.png
+cp $ASSETS_DIR_DARK/entry-disabled-toolbar.png "$MENU_TOOLBAR_DIR"/entry-disabled-toolbar-dark.png
 
-cp $DARK_ASSETS_DIR/menubar.png menubar-toolbar/menubar-dark.png
-cp $DARK_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-dark.png
+cp $ASSETS_DIR_DARK/menubar.png "$MENU_TOOLBAR_DIR"/menubar-dark.png
+cp $ASSETS_DIR_DARK/menubar_button.png "$MENU_TOOLBAR_DIR"/menubar_button-dark.png
 
-cp $DARKEST_ASSETS_DIR/entry-toolbar.png menubar-toolbar/entry-toolbar-darkest.png
-cp $DARKEST_ASSETS_DIR/entry-active-toolbar.png menubar-toolbar/entry-active-toolbar-darkest.png
-cp $DARKEST_ASSETS_DIR/entry-disabled-toolbar.png menubar-toolbar/entry-disabled-toolbar-darkest.png
+cp $ASSETS_DIR_DARKEST/entry-toolbar.png "$MENU_TOOLBAR_DIR"/entry-toolbar-darkest.png
+cp $ASSETS_DIR_DARKEST/entry-active-toolbar.png "$MENU_TOOLBAR_DIR"/entry-active-toolbar-darkest.png
+cp $ASSETS_DIR_DARKEST/entry-disabled-toolbar.png "$MENU_TOOLBAR_DIR"/entry-disabled-toolbar-darkest.png
 
-cp $DARKEST_ASSETS_DIR/menubar.png menubar-toolbar/menubar-darkest.png
-cp $DARKEST_ASSETS_DIR/menubar_button.png menubar-toolbar/menubar_button-darkest.png
+cp $ASSETS_DIR_DARKEST/menubar.png "$MENU_TOOLBAR_DIR"/menubar-darkest.png
+cp $ASSETS_DIR_DARKEST/menubar_button.png "$MENU_TOOLBAR_DIR"/menubar_button-darkest.png
 
-exit 0
