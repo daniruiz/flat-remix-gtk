3547b4df549922557996f03c375ac5b526b7f4f8
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-03-26 07:56:34 +0100

    Update build.sh script to automatize color variants generation

--- src/build.sh
+++ src/build.sh
@@ -1,53 +1,72 @@
-#!/bin/sh
+#!/bin/bash
 
-TMP="/tmp/flat-remix-gtk"
+shopt -s globstar
+
+TMP='/tmp/flat-remix-gtk'
 ASSETS_DIR=../assets-renderer/gtk3/assets
-
-rm -rf "$TMP"
-mkdir -p "$TMP"
-
-# Build CSS files
-for scss in sass/*.scss
-do
-	echo "Generate ${scss}"
-	scss --sourcemap=none -C -q "$scss" "$TMP"/"$(basename ${scss%%.scss})".css
-done
+VARIANTS=(DEFAULT Red Green Yellow)
+VARIANT_COLORS=('#367bf0' '#d41919' '#17917a' '#ffd86e')
+VARIANT_SELECTED_FONT_COLORS=('#ffffff' '#ffffff' '#ffffff' '#000000' )
+DEFAULT_COLOR='#367bf0'
 
 
-# Copy CSS files
-cp -f "$TMP"/gtk.css ../Flat-Remix-GTK/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-solid.css ../Flat-Remix-GTK-Solid/gtk-3.0/gtk.css
+function generate_variant_template {
+	variant="$1"
+	variant_color="$2"
+	for theme in variant-templates/*
+	do
+		basename=$(basename $theme)
+		cp -a "$theme" "$TMP/${variant}${basename#Flat-Remix-GTK}"
+	done
+	sed "s/${DEFAULT_COLOR}/${variant_color}/gi" -i $(find "$TMP" -type f)
+}
 
-cp -f "$TMP"/gtk-dark.css ../Flat-Remix-GTK/gtk-3.0/gtk-dark.css
-cp -f "$TMP"/gtk-dark.css ../Flat-Remix-GTK-Dark/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-dark.css ../Flat-Remix-GTK-Darker/gtk-3.0/gtk-dark.css
-cp -f "$TMP"/gtk-dark-solid.css ../Flat-Remix-GTK-Solid/gtk-3.0/gtk-dark.css
-cp -f "$TMP"/gtk-dark-solid.css ../Flat-Remix-GTK-Dark-Solid/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-dark-solid.css ../Flat-Remix-GTK-Darker-Solid/gtk-3.0/gtk-dark.css
+function generate_sass_files {(
+	variant_color="$1"
+	variant_selected_font_color="$2"
+	cd sass;
+	for scss in *.scss
+	do
+		echo "Generate ${scss}"
+		echo "\$selected_bg_color: $variant_color; \$selected_fg_color: ${variant_selected_font_color};" | \
+				cat - "$scss" | \
+				scss --sourcemap=none -C -q -s "$TMP"/"$(basename ${scss%%.scss})".css
+	done
+)}
 
-cp -f "$TMP"/gtk-darker.css ../Flat-Remix-GTK-Darker/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-darker-solid.css ../Flat-Remix-GTK-Darker-Solid/gtk-3.0/gtk.css
+function copy_css_files {
+	variant="$1"
+	cp -f "$TMP/gtk.css" "$TMP/${variant}/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-solid.css" "$TMP/${variant}-Solid/gtk-3.0/gtk.css"
 
-cp -f "$TMP"/gtk-darkest.css ../Flat-Remix-GTK-Darkest/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-darkest-solid.css ../Flat-Remix-GTK-Darkest-Solid/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-darkest-noBorder.css ../Flat-Remix-GTK-Darkest-NoBorder/gtk-3.0/gtk.css
-cp -f "$TMP"/gtk-darkest-solid-noBorder.css ../Flat-Remix-GTK-Darkest-Solid-NoBorder/gtk-3.0/gtk.css
+	cp -f "$TMP/gtk-dark.css" "$TMP/${variant}/gtk-3.0/gtk-dark.css"
+	cp -f "$TMP/gtk-dark.css" "$TMP/${variant}-Dark/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-dark.css" "$TMP/${variant}-Darker/gtk-3.0/gtk-dark.css"
+	cp -f "$TMP/gtk-dark-solid.css" "$TMP/${variant}-Solid/gtk-3.0/gtk-dark.css"
+	cp -f "$TMP/gtk-dark-solid.css" "$TMP/${variant}-Dark-Solid/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-dark-solid.css" "$TMP/${variant}-Darker-Solid/gtk-3.0/gtk-dark.css"
 
-# Generate assets
-if [[ $1 != "--no-assets" ]]
-then
-	cp -r assets-renderer "$TMP"/
+	cp -f "$TMP/gtk-darker.css" "$TMP/${variant}-Darker/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-darker-solid.css" "$TMP/${variant}-Darker-Solid/gtk-3.0/gtk.css"
+
+	cp -f "$TMP/gtk-darkest.css" "$TMP/${variant}-Darkest/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-darkest-solid.css" "$TMP/${variant}-Darkest-Solid/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-darkest-noBorder.css" "$TMP/${variant}-Darkest-NoBorder/gtk-3.0/gtk.css"
+	cp -f "$TMP/gtk-darkest-solid-noBorder.css" "$TMP/${variant}-Darkest-Solid-NoBorder/gtk-3.0/gtk.css"
+}
+
+function generate_assets {
+	rm -rf "$TMP"/assets-renderer
+	variant="$1"
+	variant_selected_font_color="$2"
+	cp -rf assets-renderer "$TMP"/
+	sed "s/${DEFAULT_COLOR}/${variant_color}/gi" -i $(find "$TMP/assets-renderer" -type f)
 	"$TMP"/assets-renderer/gtk2/render-assets.sh
 	"$TMP"/assets-renderer/gtk3/render-assets.sh
 	"$TMP"/assets-renderer/metacity/render-assets.sh
 
-	for theme in ../Flat-Remix-GTK*
+	for theme in "$TMP/$variant"*
 	do
-		rm "$theme"/gtk-3.0/assets/*
-		cp -f "$TMP"/assets-renderer/gtk3/assets/* "$theme"/gtk-3.0/assets/
-
-		rm "$theme"/gtk-2.0/assets/*
-		rm "$theme"/metacity-1/*.svg
 		case "$theme" in
 			*Darkest*)
 				cp -f "$TMP"/assets-renderer/gtk2/assets-darkest/* "$theme"/gtk-2.0/assets/
@@ -66,9 +85,30 @@ then
 				cp -f "$TMP"/assets-renderer/metacity/metacity/* "$theme"/metacity-1/
 				;;
 		esac
-
-		rm "$theme"/gtk-2.0/menubar-toolbar/*.png
 		cp -f "$TMP"/assets-renderer/gtk2/menubar-toolbar/* "$theme"/gtk-2.0/menubar-toolbar/
+		cp -f "$TMP"/assets-renderer/gtk3/assets/* "$theme"/gtk-3.0/assets/
 	done
-fi
+}
+
+
+rm -rf "$TMP"
+mkdir -p "$TMP"
+
+for i in $(seq 0 $[${#VARIANTS[*]}-1])
+do
+	variant="${VARIANTS[$i]}"
+	if [ "$variant" = 'DEFAULT' ];
+		then variant='Flat-Remix-GTK';
+		else variant="Flat-Remix-GTK-$variant"
+	fi
+	variant_color="${VARIANT_COLORS[$i]}"
+	variant_selected_font_color="${VARIANT_SELECTED_FONT_COLORS[$i]}"
+
+	generate_variant_template "$variant" "$variant_color"
+	generate_sass_files "$variant_color" "$variant_selected_font_color"
+	copy_css_files "$variant"
+	[[ $1 != "--no-assets" ]] && generate_assets "$variant" "$variant_color"
+done
+
+rsync -a --delete "$TMP"/Flat-Remix* ..
 
