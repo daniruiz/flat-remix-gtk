d3333e9922026a04c9c9f54f2e5dd85cdcf95a8f
Daniel Ruiz de Alegría <daniel@drasite.com>
2024-07-30 16:03:49 +0200

    Remove uninstall.sh too during theme files removal

--- themes/Flat-Remix-GTK-Red-Light/uninstall.sh
+++ themes/Flat-Remix-GTK-Red-Light/uninstall.sh
@@ -4,6 +4,7 @@ theme=Flat-Remix-GTK-Red-Light
 
 rm -rf ~/.config/gtk-4.0/gtk.css \
        ~/.config/gtk-4.0/assets \
+       ~/.config/gtk-4.0/uninstall.sh \
        ~/.themes/$theme/
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
